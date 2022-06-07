c52252df75d8645d1f2a7807c0271ea926093c83
Quang Nguyen <quangnm.work@gmail.com>
2022-06-07 19:02:00 +0700

    done parameters problem

--- easy/Includes/index.ts
+++ easy/Includes/index.ts
@@ -1,3 +1,5 @@
+import { Equal } from '@type-challenges/utils';
+
 type Includes<T extends unknown[], U> = T extends [
   infer firstElement,
   ...infer RestArray
@@ -6,8 +8,8 @@ type Includes<T extends unknown[], U> = T extends [
     ? true
     : Includes<RestArray, U>
   : false;
-type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>;
-type Equal<T, U> = T extends U ? true : false;
+type isPillarMen = Includes<[boolean, 2, 3, 5, 6, 7], false>;
+
 type IncludesAlter<T extends readonly any[], U> = {
   [K in keyof T]: Equal<T[K], U>;
 }[number] extends false
@@ -17,3 +19,5 @@ type isPillarMen1 = IncludesAlter<
   ['Kars', 'Esidisi', 'Wamuu', 'Santana'],
   'Dio'
 >;
+export type IsAny<T> = 0 extends 1 & T ? true : false;
+type I = boolean extends false ? true : false;
